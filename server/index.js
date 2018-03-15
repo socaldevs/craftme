a2d1e57f569656c833d287b3623414719f8268e0
Brian Duong <brndng89@gmail.com>
2018-03-15 10:57:59 -0700

    [db] sequelize instance, schema, tableCreator.js, and npm run build-db script

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const sequelize = require('../db'); 
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+sequelize.sync()
+  .then(() => {
+    console.log('DB synced');
+  });
