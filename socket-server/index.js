d154017efe927fd1a8c970bdabbecb69df704d97
Brian Duong <brndng89@gmail.com>
2018-03-17 21:34:48 -0700

    wip -- working on rooms

--- socket-server/index.js
+++ socket-server/index.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const socket = require('socket.io');
-//const http = require('http');
+
+const router = require('./routes');
+const db = require('./db');
 
 const app = express();
 const server = app.listen(3001, console.log('listening to PORT 3001'));
-//const server = http.Server(app);
-//server.listen(3001, console.log('listening to PORT 3001!))
-
-const io = socket(server);
-
+const io = socket(server); 
+// web socket connection UPGRADES the http connection
+// io is the upgraded http server
 io.on('connection', (socket) => {
+  // 'socket' var represents specific client connection
   console.log('made socket connection!', socket.id);
   
-  // socket.on('room', (room) => {
-  //   socket.join(room);
-  //   io.sockets.in(room).emit('message', 'SUUUUP');
-  // });
+  socket.on('room', (room) => {
+    socket.join(room);
+    io.sockets.in(room).emit('message', 'SUUUUP');
+  });
+
+  // handlers for that particular client only
   socket.on('chat', (data) => {
     io.sockets.emit('chat', data);
   });
+
   socket.on('typing', (data) => {
     socket.broadcast.emit('typing', data);
   });
 });
+
+//app.use(router);
 
 
 
