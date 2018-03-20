46bcc6340fc3288eff2c8887a0ca7cd6fde1e491
Brian Duong <brndng89@gmail.com>
2018-03-20 08:36:39 -0700

    [socket server] cleaned up index

--- socket-server/index.js
+++ socket-server/index.js
@@ -4,6 +4,9 @@ const parser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const { router } = require('./routes');
+const db = require('./db');
+
 const middleware = [
   helmet(),  
   parser.json(),
@@ -14,25 +17,27 @@ const middleware = [
   }),
 ];
 
-const { router } = require('./routes');
-const db = require('./db');
-
 const app = express();
 const server = app.listen(3001, console.log('listening to PORT 3001'));
-const io = socket(server); 
-// web socket connection UPGRADES the http connection
-// io is the upgraded http server
+const io = socket(server); // io is upgraded server, web socket cnxn UPGRADES http cnxn
 
-io.on('connection', (socket) => {
-  // 'socket' var represents specific client connection
+io.on('connection', (socket) => { // 'socket' var represents specific client connection
   console.log('made socket connection!', socket.id);
   
   socket.on('room', (room) => {
     socket.join(room);
+    console.log('joined room', room)
     io.sockets.in(room).emit('message', 'SUUUUP');
   });
 
+  socket.on('exit', (room) => {
+    console.log('I have left room');
+    socket.leave(room);
+  });
+
   socket.on('chat', (data) => {
+    console.log('room for chat event',data.room)
+    // console.log('whos room 1',io.sockets.adapter.rooms[1]);
     io.sockets.in(data.room).emit('chat', data);
   });
 
