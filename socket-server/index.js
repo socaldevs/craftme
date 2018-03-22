26c19e8336b46b5d74a475a61e8e04ab2a71eb57
Brian Duong <brndng89@gmail.com>
2018-03-22 10:37:14 -0700

    [cleanup] code cleanup

--- socket-server/index.js
+++ socket-server/index.js
@@ -10,7 +10,7 @@ const mongoDB = require('./db/mongoDB');
 const PORT = 3001;
 const app = express();
 const server = app.listen(PORT, console.log(`Listening to PORT ${PORT}!`));
-const io = socket(server); // io is upgraded server, web socket cnxn UPGRADES http cnxn
+const io = socket(server); // io is server upgraded w/ web socket connection
 const ExpressPeerServer = require('peer').ExpressPeerServer;
 
 const options = {
@@ -27,47 +27,35 @@ const middleware = [
   }),
 ];
 
-io.on('connection', (socket) => { // 'socket' var represents specific client connection
+io.on('connection', (socket) => { // 'socket' represents specific client connection
   console.log('made SOCKET connection!', socket.id);
   
   socket.on('room', (room) => {
     socket.join(room);
     io.sockets.in(room).emit('confirmation', `Someone has joined room ${room}`);
   });
-  
   socket.on('renderchat', (data) => {
     io.sockets.in(data.room).emit('renderchat', data.messages);
   });
-
   socket.on('exit', (room) => {
     socket.leave(room);
   });
-
   socket.on('chat', (data) => {
-    // console.log(io.sockets.adapter.rooms[1]);
+    // console.log(io.sockets.adapter.rooms[roomId]);
     io.sockets.in(data.room).emit('chat', data);
   });
-
   socket.on('typing', (data) => {
     socket.broadcast.to(data.room).emit('typing', data.feedback);
   });
-
-  // socket.on('peer', (data) => {
-  //   socket.broadcast.to(data.room).emit('peer', data.id);
-  //   console.log('---LOOK HERE', data.id);
-  // });
   socket.on('getOtherPeerId', (data) => {
-    console.log('getOtherPeerId', data);
     socket.broadcast.to(data).emit('getOtherPeerId', 'test');
   });
-
   socket.on('fetchedPeerId', (data) => {
-    console.log('fetchedPeerId', data);
     socket.broadcast.to(data.room).emit('fetchedPeerId', data.peerId);
   });
 });
 
-server.on('connection', function(id) { console.log('made PEER connection!'); });
+server.on('connection', (id) => console.log('made PEER connection!'));
 
 app.use(...middleware);
 app.use('/api', ExpressPeerServer(server, options));
