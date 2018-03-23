851b668a0c5a485bc2a76b856fa64525a13481e8
fireArthur <thefirearthur@gmail.com>
2018-03-22 19:09:39 -0700

    [Search] builds queries for craft search

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -62,12 +62,40 @@ module.exports = {
 
   getIdByUsername: async (req, res) => {
     try {
-      let { username } = req.params;
-      let id = await db.User.findOne({ where: { username } });
+      const { username } = req.params;
+      const id = await db.User.findOne({ where: { username } });
       res.status(201).send(id);
     } catch (error) {
       console.log('Error with getIdByUsername', error);
       return;
     }
-  }
+  },
+  getAllCrafts: async (req, res) => {
+    try {
+      const crafts = await db.Craft.findAll({});
+      res.status(200).send(crafts);
+    } catch (error) {
+      console.log('Error with getIdByUsername', error);
+      return;
+    }
+  },
+  getCraftTeachers: async (req, res) => {
+    try {
+      const { craftId } = req.params;
+      const teachers = await db.Craft.findAll({
+        where: {
+          id:craftId
+        },
+        include: [{
+            model: db.User,
+            // as: 'userId'  
+          }]  
+      });
+       res.status(200).send(teachers);
+    } catch (error) {
+      console.log('Error with getCraftTeachers', error);
+      return;
+    }
+  },
+
 };
