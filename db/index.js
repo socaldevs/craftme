af1d4616e84eaa332e0303e345184fadada26c17
Eddie Huang <ehuange@gmail.com>
2018-03-23 20:04:28 -0700

    [Rebase] - Commit for rebase/pull

--- db/index.js
+++ db/index.js
@@ -1,13 +1,8 @@
 const Sequelize = require('sequelize');
 
 // setting up the connection configurations
-<<<<<<< HEAD
-const sequelize = new Sequelize('postgres', 'root', '', {
-  host: 'localhost',  // we might have to change this when deployment
-=======
 const sequelize = new Sequelize('skills', 'root', '1234', {
   host: 'localhost', // we might have to change this when deployment
->>>>>>> [Messaging and Lessons] - Builds fully functional routes to front end
   dialect: 'postgres',
   pool: {
     max: 5,
@@ -15,11 +10,7 @@ const sequelize = new Sequelize('skills', 'root', '1234', {
     acquire: 30000,
     idle: 10000
   },
-<<<<<<< HEAD
-  port: 5432 || 5433,
-=======
   port: 5433,
->>>>>>> [Rebase] - commit for rebase
   operatorsAliases: false
 });
 
