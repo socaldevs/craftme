676315da56105015feaef84ee20089e450aa2bab
Brian Duong <brndng89@gmail.com>
2018-03-26 15:51:06 -0700

    [userControllers] added getTranslation and getLanguageList

--- server/languageTranslator.js
+++ server/languageTranslator.js
@@ -1,35 +1,34 @@
-var LanguageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
+const LanguageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
 
-var languageTranslator = new LanguageTranslatorV2({
-  username: '<username>',
-  password: '<password>',
+const languageTranslator = new LanguageTranslatorV2({
+  username: '332be775-f147-43ce-9afb-a955b3855936',
+  password: 'VdaWbwSTz75V',
   url: 'https://gateway.watsonplatform.net/language-translator/api/'
 });
 
-languageTranslator.translate(
-  {
-    text: 'A sentence must have a verb',
-    source: 'en',
-    target: 'es'
-  },
-  function(err, translation) {
-    if (err)  {
-      console.log('error:', err);
-    } else  {
-      console.log(JSON.stringify(translation, null, 2));
-  }
-);
+let parameters = {
+  text: 'Goodbye',
+  model_id: 'en-es'
+};
 
-languageTranslator.identify(
-  {
-    text:
-      'The language translator service takes text input and identifies the language used.'
-  },
-  function(err, language) {
-    if (err)  {
-      console.log('error:', err);
-    } else {
-      console.log(JSON.stringify(language, null, 2));
-    }
-  }
-);
+// languageTranslator.translate(
+//   parameters,
+//   (error, response) => {
+//     if (error)
+//       console.log(error)
+//     else
+//       console.log(JSON.stringify(response, null, 2));
+//   }
+// );
+
+// languageTranslator.listIdentifiableLanguages(
+//   {},
+//   (err, response) => {
+//     if (err)
+//       console.log(err)
+//     else
+//       console.log(JSON.stringify(response, null, 2));
+//   }
+// );
+
+module.exports = { languageTranslator };
