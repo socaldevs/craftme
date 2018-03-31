13eb758850ecce03f49a1c421e14f59cc851f9a6
fireArthur <thefirearthur@gmail.com>
2018-03-31 16:08:18 -0700

    [Credentials] modifies credentials to fit computer

--- db/index.js
+++ db/index.js
@@ -6,7 +6,11 @@ env.config({path: (ENV)});
 console.log("database path: ",ENV)
 
 //for Brian: 'postgres', process.env.DB_USER, ''
-const sequelize = new Sequelize('postgres', process.env.DB_USER, '', {
+// please make changes specific to your machine on the env file or 
+// comment out how the connection settings were before you edited it
+// like so:
+// const sequelize = new Sequelize('postgres', process.env.DB_USER, '', {
+const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   pool: {
