33ed93d14b01b2f96c664a5e0012bed5b02b8212
Brian Duong <brndng89@gmail.com>
2018-04-03 11:49:20 -0700

    [submitCraft] back end

--- db/index.js
+++ db/index.js
@@ -5,12 +5,14 @@ const ENV = path.resolve(__dirname, '../.env')
 env.config({path: (ENV)});
 console.log("database path: ",ENV)
 
-//for Brian: 'postgres', process.env.DB_USER, ''
+
 // please make changes specific to your machine on the env file or 
 // comment out how the connection settings were before you edited it
 // like so:
 // const sequelize = new Sequelize('postgres', process.env.DB_USER, '', {
-const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+// BEFORE: const sequelize = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+// for Brian: 'postgres', process.env.DB_USER, ''
+const sequelize = new Sequelize( 'postgres', process.env.DB_USER, '', {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   pool: {
--- server/controllers/craftsController.js
+++ server/controllers/craftsController.js
@@ -1,14 +1,14 @@
 const db = require('../../db/schema.js');
 
 const submitCraft = async (req, res) => {
-  const { name, description, teacher_id } = req.body;
+  const { name, description, userId } = req.body;
   try {
     const craft = await db.Craft.create({
       name,
       description,
     });
     const association = await db.CraftTeacher.create({
-      userId: teacher_id,
+      userId,
       craftId: craft.dataValues.id,
     });
     res.send(association);
